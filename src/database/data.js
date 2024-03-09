cea83be81d09a31cdb535263f089e352a59a1de5
Jan romel <janjancaranguian@gmail.com>
2024-03-10 07:09:16 +0800

    Make an API imitation to temporarily store data

---
+++ src/database/data.js
@@ -0,0 +1,24 @@
+export const data = [
+  {
+    logoName: "google",
+    logo: "https://static.vecteezy.com/system/resources/thumbnails/012/871/371/small/google-search-icon-google-product-illustration-free-png.png",
+    email: "Janjancaranguian@gmail.com",
+    username: "jan",
+    password: "wubalubadubdub"
+  }, 
+  {
+    logoName: "youtube",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png",
+    email: "Janjancaranguian@gmail.com",
+    username: "leafboo",
+    password: "sugnAD12"
+  }, 
+  {
+    logoName: "twitter",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/512px-Logo_of_Twitter.svg.png",
+    email: "Janjancaranguian@gmail.com",
+    username: "coldsalami",
+    password: "suW31c"
+  }
+
+]
